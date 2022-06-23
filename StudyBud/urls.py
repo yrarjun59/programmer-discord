27491293138f108676b8db15d33cff0d16ca43e7
Arjun Thapa <yrarjun59@gmail.com>
2022-06-23 14:17:55 +0545

    slight

--- StudyBud/urls.py
+++ StudyBud/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
 
-    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
 

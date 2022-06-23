6431ab12e9bedaeaba28025d6469ebdf23972120
Arjun Thapa <yrarjun59@gmail.com>
2022-06-23 14:21:50 +0545

    slight

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

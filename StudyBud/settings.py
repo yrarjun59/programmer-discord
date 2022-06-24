c9ca28e868a83506311dd84a7b0a38fb4ba3772c
Arjun Thapa <yrarjun59@gmail.com>
2022-06-24 12:50:08 +0545

    updatd

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

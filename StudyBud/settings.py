651f3447ca4d01dee857749f231a89232a152810
Arjun Thapa <yrarjun59@gmail.com>
2022-06-24 13:02:51 +0545

    making better

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-sumh^stmnd1cyfk12u23mcc(^spg+hu^7xj&%7+!h3)jd#*3a#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['localhost', 'yrprogrammer-discord.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -48,7 +47,6 @@ AUTH_USER_MODEL = 'base.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,16 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
